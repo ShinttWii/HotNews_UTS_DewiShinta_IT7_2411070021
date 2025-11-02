fcd3e1a8cd64931adc6de19e8bb3a937989f2e18
ShinttWii <28.dewi.28.shinta@gmail.com>
2025-11-02 19:32:19 +0700

    Update news.js

--- api/news.js
+++ api/news.js
@@ -8,6 +8,6 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ message: "Error fetching news", error });
+    res.status(500).json({ message: "Error fetching news", error: error.message });
   }
 }
