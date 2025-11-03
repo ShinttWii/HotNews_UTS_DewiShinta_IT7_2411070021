39f01cf24bd7386a6e9b49177a0b678746ce1938
ShinttWii <28.dewi.28.shinta@gmail.com>
2025-11-03 07:10:21 +0700

    Create news.js

---
+++ api/news.js
@@ -0,0 +1,22 @@
+import fetch from "node-fetch";
+
+const apiKey = "ce35ce93c19f45689d2fea0c01902bb1";
+
+export default async function handler(req, res) {
+  const category = req.query.category || "general";
+
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=id&category=${category}&apiKey=${apiKey}`
+    );
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: "Failed to fetch news" });
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
